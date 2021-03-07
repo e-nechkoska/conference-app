6e52d799e523d73b1480cff0a0b70f5c5aa97442
Emilija Nechkoska <Emilija.Nechkoska@netcetera.com>
2021-03-07 19:32:09 +0100

    Change the prefix of the components

--- client/conference-app/src/app/app.component.ts
+++ client/conference-app/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ca-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'conference-app';
+  hi = 'HIHIHI';
+
+  getName() {
+    return 'test tests';
+  }
 }
