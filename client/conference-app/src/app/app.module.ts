97a546935a134fe49819bc381f53a19b3395db95
Emilija Nechkoska <Emilija.Nechkoska@netcetera.com>
2021-03-07 20:40:41 +0100

    Cleanup

--- client/conference-app/src/app/app.module.ts
+++ client/conference-app/src/app/app.module.ts
@@ -3,20 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SpeakerService } from './speaker.service';
-import { SpeakersComponent } from './speakers.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SpeakersComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [
-    SpeakerService
   ],
   bootstrap: [
     AppComponent
