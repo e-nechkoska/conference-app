71700dbeb0522227da7118ce31ce9d6f065c8f4a
Emilija Nechkoska <Emilija.Nechkoska@netcetera.com>
2021-03-19 21:35:49 +0100

    Use snapshot to get the resolved sessions

--- client/conference-app/src/app/sessions/pages/sessions-page.component.ts
+++ client/conference-app/src/app/sessions/pages/sessions-page.component.ts
@@ -1,31 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Session } from '../models/session';
-import { Observable, of } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'ca-sessions',
   templateUrl: './sessions-page.component.html',
   styleUrls: ['./sessions-page.component.css']
 })
-export class SessionsPageComponent {
-  sessions$: Observable<Session[]> = of([]);
-  selectedSession$: Observable<Session> = of({} as Session);
+export class SessionsPageComponent implements OnInit {
+  sessions: Session[];
   selectedSession: Session;
 
   constructor(private route: ActivatedRoute) {
-    this.sessions$ = route.data.pipe(
-      map(data => data.sessions)
-    );
+  }
 
-    this.selectedSession$ = this.sessions$.pipe(
-      map(sessions => sessions[0])
-    );
+  ngOnInit() {
+    this.sessions = this.route.snapshot.data.sessions;
+    this.selectedSession = this.sessions[0];
   }
 
   onSelectionChanged(session: Session) {
-    this.selectedSession$ = of(session);
-
+    this.selectedSession = session;
   }
 }
